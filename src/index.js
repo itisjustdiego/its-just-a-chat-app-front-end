89b17b91fef2bd53ad1934b00b74a18ddb61327b
Jason Decker <jasondecker@Portatile-Flatiron.local>
2017-11-16 13:56:21 -0500

    Add message to dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("this JS code runs")
+
+
+document.addEventListener('DOMContentLoaded',function (){
+  const messageForm = document.getElementById('message-form')
+  const messageInput = document.getElementById('message-input')
+  const chatContainer = document.getElementById('chat-container')
+  console.log(messageForm)
+  console.log(messageInput)
+  console.log(chatContainer)
+  messageForm.addEventListener('submit',function(e) {
+    e.preventDefault()
+    const message = messageInput.value
+    console.log(message)
+    messageInput.value = ''
+    const messageLi = document.createElement('li')
+    // is message something that look like 'message' or '<li>messasge</li>'
+    messageLi.innerText=message
+    console.log(messageLi)
+    chatContainer.appendChild(messageLi)
+  })
+})
